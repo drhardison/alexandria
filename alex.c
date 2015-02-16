03b726348e33997673dec3b1c7463734e5562943
drhardison <joseph.m.hagan@wmich.edu>
2015-02-16 11:10:34 -0500

    added library files

---
+++ alex.c
@@ -0,0 +1,49 @@
+//C Library of Alexandria
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <fcntl.h>  
+#include "joe.h"
+
+void println(char * line){
+	fprintf(stdout, "%s\n", line);
+}
+
+void printerr(char * line){
+	fprintf(stderr, "%s\n", line);
+}
+
+void printerrno(int err){
+	fprintf(stderr,"%s\n", strerror(err));
+}
+
+void printint(char * begin, int num, char * end){
+	int size = sizeof(char) + sizeof(*begin) + sizeof(*end);
+	char * line = (char *)malloc(size);
+	int error = sprintf(line, "%s%d%s", begin, num, end);
+	if(error>=0){
+		fprintf(stdout, "%s\n", line);
+	}
+	else{
+		printerr("Was Not Able To Print Int String. Double Check the Parameters.");
+	}
+}
+
+void clear(){
+	pid_t pid = fork();
+	if (pid != 0) 
+	{
+		wait(NULL);
+	} 
+	else
+	{
+		execlp("clear","clear",(char *)0);
+		exit(0);
+	}
+}
---
+++ alex.h
@@ -0,0 +1,22 @@
+//C Library of Alexandria - header file
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <fcntl.h>  
+#define true 1
+#define false 0
+
+typedef int bool;
+typedef int boolean;
+
+void println(char *); 	// Prints the string with \n added.
+void printerr(char *);	// Prints the error string that you send it terminated with a \n.
+void printerrno(int);	// Takes the errno, and prints the associated error with a \n.
+void printint(char *, int, char *);	// An easy way to print a line with an int. You can put text before or after if you want.
+void clear();	//Clears the terminal output to give you a fresh screen.
